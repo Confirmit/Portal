0f7f3297ab467424f3e611b9732783f3f4a33ee8
Grimranger <alovchenkov@gmail.com>
2015-03-20 14:17:05 +0300

    Confirmit/Portal/#19-NullReferenceException (message was changed. Name and emails of administrators can be modified from web config)

--- Portal/ErrorPages/AccessDenied.aspx.cs
+++ Portal/ErrorPages/AccessDenied.aspx.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.Web.Configuration;
 
 public partial class ErrorPages_AccessDenied : BaseWebPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        if (!string.IsNullOrEmpty(this.Page.Request.QueryString["aspxerrorpath"]))
-        {
-            lblErrorDescription.Text = 
-                string.Format(lblErrorDescription.Text, this.Page.Request.QueryString["aspxerrorpath"]);
-        }
-        else
-        {
-            lblErrorDescription.Text =
-                string.Format(lblErrorDescription.Text, string.Empty);
-        }
+        string adminMail = WebConfigurationManager.AppSettings["AdminsMails"];
+        string adminName = WebConfigurationManager.AppSettings["AdminsNames" + CultureInfo.CurrentCulture.TwoLetterISOLanguageName];
+        string message = string.Format(lblErrorDescription.Text, adminMail, adminName);
+     
+        lblErrorDescription.Text = message;
+
+
+                //string.Format(lblErrorDescription.Text, userName);
     }
 }
 
