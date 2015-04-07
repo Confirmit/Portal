25e5651aff34e134dc2957983cb1acfb4693e229
NickKuzmin <kuzmin.nick@rambler.ru>
2015-04-06 20:15:35 -0700

    Confirmit/Portal#37 [MigrationFromPersonDataSourceToBasePlainObject] Method 'SiteProvider.Users.GetFilteredUsers' with sql-query was changed on method from class 'BasePlainObject'.

--- Libraries/PortalLibraries/PortalLib/BusinessObjects/Persons/PersonDataSource.cs
+++ Libraries/PortalLibraries/PortalLib/BusinessObjects/Persons/PersonDataSource.cs
@@ -3,7 +3,7 @@
 using ConfirmIt.PortalLib.BusinessObjects.Persons.Filter;
 using ConfirmIt.PortalLib.DAL;
 using ConfirmIt.PortalLib.DataSource;
-
+using Core;
 using UlterSystems.PortalLib.BusinessObjects;
 
 namespace ConfirmIt.PortalLib.BusinessObjects.Persons
@@ -14,7 +14,16 @@ namespace ConfirmIt.PortalLib.BusinessObjects.Persons
 
         public override IList<Person> Select(string SortExpression, int maximumRows, int startRowIndex) 
         {
-            return SiteProvider.Users.GetFilteredUsers(SortExpression, maximumRows, startRowIndex, (PersonsFilter)Filter);
+            var isAscendingOrder = true;
+            if (SortExpression.Contains(" DESC"))
+            {
+                SortExpression = SortExpression.Substring(0, SortExpression.LastIndexOf(" DESC"));
+                isAscendingOrder = false;
+            }
+            if (SortExpression == "")
+                SortExpression = "LastName";
+            var pagingResult = BasePlainObject.GetObjectsPage(typeof(Person), new PagingArgs(startRowIndex / maximumRows, maximumRows, SortExpression, isAscendingOrder));
+            return (IList<Person>)pagingResult.Result;
         }
 
         public override int SelectCount()
