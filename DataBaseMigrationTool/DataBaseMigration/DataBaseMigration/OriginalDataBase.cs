cc1f83e4cf40b392c4558ebcfe6e85f41fe061c9
GrimRanger <alovchenkov@gmail.com>
2015-04-09 23:16:08 +0300

    Confirmit/Portal/#34-Database_migration_tool (migrator was added to folder)

---
+++ DataBaseMigrationTool/DataBaseMigration/DataBaseMigration/OriginalDataBase.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace DataBaseMigration
+{
+    [Migration(0)]
+    public class OriginalDataBase : Migration
+    {
+        public override void Up()
+        {
+         
+        }
+
+        public override void Down()
+        {
+           
+        }
+    }
+}
