437c24e40e35d2103e24f8ed15864a9f31abb964
prototype9449 <prototype9449@yandex.ru>
2015-04-20 18:33:24 +0300

    Confirmit/Portal#45 Classes for working with rules and groups were added

---
+++ Libraries/PortalLibraries/PortalLib/BusinessObjects/Rules/UserGroup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Core;
+using Core.DB;
+using Core.ORM.Attributes;
+using UlterSystems.PortalLib.DB;
+
+namespace ConfirmIt.PortalLib.Rules
+{
+    [DBTable("UserGroups")]
+    public class UserGroup : ObjectDataBase
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private string _description = string.Empty;
+        
+        public List<int> UsersId { get; set; }
+
+        [DBRead("Description")]
+        public string Description
+        {
+            [DebuggerStepThrough]
+            get { return _description; }
+            [DebuggerStepThrough]
+            set { _description = value; }
+        }
+
+        public UserGroup(string description)
+        {
+            Description = description;
+            base.ResloveConnection();
+        }
+
+        public UserGroup(string description, List<int> usersId) : this(description)
+        {
+            UsersId = new List<int>(usersId);
+        }
+
+        public void AddUserId(int id)
+        {
+            UsersId.Add(id);
+        }
+
+        public override void Save()
+        {
+
+            base.Save();
+
+            using (SqlConnection connection = new SqlConnection(Connection))
+            {
+                connection.Open();
+
+                foreach (var userid in UsersId)
+                {
+                    SqlCommand command = connection.CreateCommand();
+
+                    command.CommandText =
+                        string.Format("INSERT INTO {0} (idUser, idUserGroup) VALUES  (@idUser, @idUserGroup)", NameTableAccord);
+                    command.Parameters.AddWithValue("@idUser", userid);
+                    command.Parameters.AddWithValue("@idUserGroup", ID);
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+
+        public override string NameTableAccord
+        {
+            get { return "GroupingUsers"; }
+        }
+    }
+}
