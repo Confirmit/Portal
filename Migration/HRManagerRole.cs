89b7eedf1ea6b0c38cce812786c9f536f9a0731a
GrimRanger <alovchenkov@gmail.com>
2015-04-29 15:05:05 +0300

    Confirmit/Portal/#34-Database_migration_tool (TranslitirationMigrate was added)

---
+++ Migration/HRManagerRole.cs
@@ -0,0 +1,35 @@
+﻿using Core;
+using FluentMigrator;
+
+namespace Migration
+{
+    [Migration(3)]
+    public class HRManagerRole : FluentMigrator.Migration
+    {
+        readonly MLText _name = new MLText("en", "HR Managers", "ru", "Менеджеры по персоналу");
+        readonly MLText _description = new MLText("en", "HR Managers", "ru", "Менеджеры по персоналу");
+        private const string RoleId = "HRManager";
+
+        private object GetSettings()
+        {
+            return new
+            {
+                GroupID = RoleId,
+                Name = _name.ToXMLString(),
+                Description = _description.ToXMLString()
+            };
+        }
+
+        public override void Up()
+        {
+            if (Schema.Table("Groups").Exists())
+                Insert.IntoTable("Groups").Row(GetSettings());
+        }
+
+        public override void Down()
+        {
+            if (Schema.Table("Groups").Exists())
+                Delete.FromTable("Groups").Row(new { GroupId = RoleId }); 
+        }
+    }
+}
