fa49a4175696f83e866eb3fb21c6d93dd1ac5c5f
prototype9449 <prototype9449@yandex.ru>
2015-05-04 14:33:38 +0300

    Confirmit/Portal#45 Added two unfinished implementers of rules : Notification the last user in some groups and rule, which don't allow send report about some users to Moscow

---
+++ Libraries/PortalLibraries/PortalLib/BusinessObjects/Implementations of rules/NotReportedToMoscowImplementer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConfirmIt.PortalLib.BusinessObjects.Rules.Interfaces;
+using ConfirmIt.PortalLib.BusinessObjects.Rules.Interfaces_of_providers_of_rules;
+
+namespace ConfirmIt.PortalLib.BusinessObjects.Implementations_of_rules
+{
+    public class NotReportedToMoscowImplementer
+    {
+        private List<IRule> _rules;
+        public NotReportedToMoscowImplementer(INotReportingToMoscowProvider providerRules)
+        {
+            _rules = providerRules.GetRules();
+        }
+    }
+}
---
+++ Libraries/PortalLibraries/PortalLib/BusinessObjects/Implementations of rules/NotificationLastUserImplementer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Provider;
+using System.Linq;
+using System.Text;
+using ConfirmIt.PortalLib.BusinessObjects.Rules.Interfaces;
+using ConfirmIt.PortalLib.BusinessObjects.Rules.Interfaces_of_providers_of_rules;
+
+namespace ConfirmIt.PortalLib.BusinessObjects.Implementations_of_rules
+{
+    public class NotificationLastUserImplementer
+    {
+        private List<INotificationLastUser> _rules;
+
+        public int UserId { get; private set; }
+
+        public NotificationLastUserImplementer(INotificationLastUserProvider providerRules, int userId)
+        {
+            _rules = providerRules.GetRules();
+            UserId = userId;
+        }
+    }
+}
