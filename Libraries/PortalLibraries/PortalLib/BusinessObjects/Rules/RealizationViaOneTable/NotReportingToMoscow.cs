35d0261598f5fbc9c5691fee87adcec2dd80600c
prototype9449 <prototype9449@yandex.ru>
2015-05-04 10:49:19 +0300

    Confirmit/Portal#45 Rule about report to Moscow

---
+++ Libraries/PortalLibraries/PortalLib/BusinessObjects/Rules/RealizationViaOneTable/NotReportingToMoscow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfirmIt.PortalLib.BusinessObjects.Rules.RealizationViaOneTable
+{
+    public class NotReportingToMoscow : Rule
+    {
+        protected override void LoadToXml()
+        {
+            _xmlInformation = string.Empty;
+        }
+
+        protected override void LoadFromXlm()
+        {
+            _xmlInformation = string.Empty;
+        }
+
+        public override int GetIdType()
+        {
+            return 4;
+        }
+
+        public NotReportingToMoscow()
+        {
+            GroupsId = new List<int>();
+        }
+
+        public NotReportingToMoscow(List<int> groupsId)
+        {
+            GroupsId = new List<int>(groupsId);
+        }
+
+    }
+}
