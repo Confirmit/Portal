78ddf82d4d4762de83997f61b66d906698aa04d7
NickKuzmin <kuzmin.nick@rambler.ru>
2015-05-13 18:28:41 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Added fourth column with operations in UserList.ascx

--- Portal/Controls/Users/UsersList.ascx
+++ Portal/Controls/Users/UsersList.ascx
@@ -1,17 +1,47 @@
 <%@ Control Language="C#" AutoEventWireup="true" Inherits="Controls_UsersList" CodeBehind="UsersList.ascx.cs" %>
 
 <asp:GridView ID="GridUsersList" runat="server" AllowSorting="true"
-    AutoGenerateColumns="False" OnSorting="SortingCommand_Click" Width="90%">
+    AutoGenerateColumns="False" OnSorting="SortingCommand_Click" 
+    OnRowDataBound="OnUserInfoBound" Width="90%">
     <Columns>
         <asp:BoundField DataField="UserID" HeaderText="User ID:" SortExpression="ID" meta:resourcekey="hUserID">
-            <ItemStyle Width="30%" BorderStyle="Solid" BorderWidth="1"/>
+            <ItemStyle BorderStyle="Solid" BorderWidth="1" />
         </asp:BoundField>
-        <asp:HyperLinkField DataTextField="UserName" DataNavigateUrlFields="UserID" DataNavigateUrlFormatString="../../UsersList/UsersInfo.aspx?UserID={0}" HeaderText="User Name:" 
+        <asp:HyperLinkField DataTextField="UserName" DataNavigateUrlFields="UserID" DataNavigateUrlFormatString="../../UsersList/UsersInfo.aspx?UserID={0}" HeaderText="User Name:"
             SortExpression="LastName" meta:resourcekey="hUser" />
         <asp:BoundField DataField="Status" HeaderText="Status:" meta:resourcekey="hStatus">
-            <ItemStyle Width="30%" BorderStyle="Solid" BorderWidth="1"/>
+            <ItemStyle BorderStyle="Solid" BorderWidth="1" />
         </asp:BoundField>
+        <asp:TemplateField meta:resourcekey="hOperations">
+            <ItemTemplate>
+                <asp:ImageButton ID="btnIll" runat="server"
+                    meta:resourcekey="btnIll"
+                    OnClick="OnSetIll"
+                    ImageUrl="~/Images/Ill.ico"
+                    Height="17" Width="17" />
+                <asp:ImageButton ID="btnTrustIll" runat="server"
+                    meta:resourcekey="btnTrustIll"
+                    OnClick="OnSetTrustIll"
+                    ImageUrl="~/Images/TrustIll.ico"
+                    Height="17" Width="17" />
+                <asp:ImageButton ID="btnBusinessTrip" runat="server"
+                    meta:resourcekey="btnBusinessTrip"
+                    OnClick="OnSetBusinessTrip"
+                    ImageUrl="~/Images/BusinessTrip.ico"
+                    Height="17" Width="17" />
+                <asp:ImageButton ID="btnVacation" runat="server"
+                    meta:resourcekey="btnVacation"
+                    OnClick="OnSetVacation"
+                    ImageUrl="~/Images/Vacation.ico"
+                    Height="17" Width="17" />
+                <asp:ImageButton ID="btnLesson" runat="server"
+                    meta:resourcekey="btnLesson"
+                    OnClick="OnSetLesson"
+                    ImageUrl="~/Images/lesson.png"
+                    Height="17" Width="17" />
+            </ItemTemplate>
+        </asp:TemplateField>
     </Columns>
-    <RowStyle HorizontalAlign="Center" BorderWidth="1px" />
+    <RowStyle HorizontalAlign="Center" BorderWidth="1px" BorderStyle="Solid"/>
 </asp:GridView>
 <asp:Label runat="server" ID="lblException" CssClass="control-errorlabel" Visible="false" />
--- Portal/Controls/Users/UsersList.ascx.cs
+++ Portal/Controls/Users/UsersList.ascx.cs
@@ -1,40 +1,73 @@
-using System;
+п»їusing System;
 using System.Web.UI.WebControls;
+using ConfirmIt.PortalLib.BAL;
 using Core;
 using UlterSystems.PortalLib.BusinessObjects;
 
 public partial class Controls_UsersList : BaseUserControl
 {
-	#region Свойства
+    public DateTime Date
+    {
+        get
+        {
+            return ViewState["Date"] == null
+                       ? DateTime.Today
+                       : (DateTime)ViewState["Date"];
+        }
+        set
+        {
+            ViewState["Date"] = value;
+        }
+    }
 
-	/// <summary>
-	/// Дата, за которую отображаются события.
-	/// </summary>
-	public DateTime Date
-	{
-		get
-		{
-			return ViewState["Date"] == null
-					   ? DateTime.Today
-					   : (DateTime) ViewState["Date"];
-		}
-		set
-		{
-			ViewState["Date"] = value;
-		}
-	}
-
-	#endregion
-
-	protected void Page_Load(object sender, EventArgs e)
-	{
+    protected void Page_Load(object sender, EventArgs e)
+    {
         if (!IsPostBack)
         {
             GridUsersList.DataSource = UserList.GetStatusesList(Date, isDescendingSortDirection: true, propertyName: "LastName");
             GridUsersList.DataBind();
             ViewState["CurrentGridViewSortEventSerializableArgs"] = new GridViewSortEventSerializableArgs("LastName", SortDirection.Ascending);
         }
-	}
+
+        if (!Page.CurrentUser.IsInRole(RolesEnum.Administrator))
+            GridUsersList.Columns[3].Visible = false;
+    }
+
+    /// <summary>
+    /// ГЏГ°ГЁГўГїГ§ГЄГ  Г¤Г Г­Г­Г»Гµ ГЇГ®Г«ГјГ§Г®ГўГ ГІГҐГ«ГҐГ© ГЄ ГЅГ«ГҐГ¬ГҐГ­ГІГ Г¬ ГіГЇГ°Г ГўГ«ГҐГ­ГЁГї.
+    /// </summary>
+    protected void OnUserInfoBound(object sender, GridViewRowEventArgs gridViewRowEventArgs)
+    {
+        var rowDataItem = gridViewRowEventArgs.Row.DataItem;
+        if (!(rowDataItem is UserStatusInfo))
+            return;
+
+        var userStatusInfo = (UserStatusInfo)rowDataItem;
+
+        if (Page.CurrentUser.ID.HasValue && userStatusInfo.UserID == Page.CurrentUser.ID.Value)
+            gridViewRowEventArgs.Row.CssClass = "gridview-selectedrow";
+
+        var imageButton = (ImageButton)gridViewRowEventArgs.Row.FindControl("btnIll");
+        if (imageButton != null)
+            imageButton.CommandArgument = userStatusInfo.UserID.ToString();
+
+        imageButton = (ImageButton)gridViewRowEventArgs.Row.FindControl("btnTrustIll");
+        if (imageButton != null)
+            imageButton.CommandArgument = userStatusInfo.UserID.ToString();
+
+        imageButton = (ImageButton)gridViewRowEventArgs.Row.FindControl("btnBusinessTrip");
+        if (imageButton != null)
+            imageButton.CommandArgument = userStatusInfo.UserID.ToString();
+
+        imageButton = (ImageButton)gridViewRowEventArgs.Row.FindControl("btnVacation");
+        if (imageButton != null)
+            imageButton.CommandArgument = userStatusInfo.UserID.ToString();
+
+        imageButton = (ImageButton)gridViewRowEventArgs.Row.FindControl("btnLesson");
+        if (imageButton != null)
+            imageButton.CommandArgument = userStatusInfo.UserID.ToString();
+    }
+
 
     protected void SortingCommand_Click(object sender, GridViewSortEventArgs e)
     {
@@ -68,5 +101,85 @@ public partial class Controls_UsersList : BaseUserControl
             else
                 column.HeaderStyle.CssClass = "";
         }
+    }
+
+    protected virtual void OnSetIll(object sender, EventArgs e)
+    {
+        CreateAbsenceEvent(sender, WorkEventType.Ill);
+    }
+
+    protected virtual void OnSetTrustIll(object sender, EventArgs e)
+    {
+        CreateAbsenceEvent(sender, WorkEventType.TrustIll);
+    }
+
+    protected virtual void OnSetBusinessTrip(object sender, EventArgs e)
+    {
+        CreateAbsenceEvent(sender, WorkEventType.BusinessTrip);
+    }
+
+    protected virtual void OnSetVacation(object sender, EventArgs e)
+    {
+        CreateAbsenceEvent(sender, WorkEventType.Vacation);
+    }
+
+    protected void OnSetLesson(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!(sender is ImageButton))
+                return;
+
+            var imageButton = (ImageButton)sender;
+
+            int userID;
+            if (!Int32.TryParse(imageButton.CommandArgument, out userID))
+                return;
+
+            var userEvents = new UserWorkEvents(userID);
+            var duration = TimeSpan.FromMinutes(45);
+            userEvents.AddLatestClosedWorkEvent(duration, WorkEventType.TimeOff);
+
+            RefreshTable();
+        }
+        catch (Exception ex)
+        {
+            lblException.Text = ex.Message;
+            lblException.Visible = true;
+        }
+    }
+
+    private void CreateAbsenceEvent(Object sender, WorkEventType eventType)
+    {
+        try
+        {
+            if (!(sender is ImageButton))
+                return;
+
+            var imageButton = (ImageButton)sender;
+
+            int userID;
+            if (!Int32.TryParse(imageButton.CommandArgument, out userID))
+                return;
+
+            var userEvents = new UserWorkEvents(userID);
+            userEvents.CreateAbsenceEvent(eventType, Date);
+
+            RefreshTable();
+        }
+        catch (Exception ex)
+        {
+            lblException.Text = ex.Message;
+            lblException.Visible = true;
+        }
+    }
+
+    private void RefreshTable()
+    {
+        var gridViewSortEventArgs = (GridViewSortEventSerializableArgs)ViewState["CurrentGridViewSortEventSerializableArgs"];
+        var sortedUsers = UserList.GetStatusesList(Date,
+            gridViewSortEventArgs.CurrentDirection == SortDirection.Ascending, gridViewSortEventArgs.SortExpression);
+        GridUsersList.DataSource = sortedUsers;
+        GridUsersList.DataBind();
     }
 }
