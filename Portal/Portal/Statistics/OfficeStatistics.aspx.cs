05d92dec47a9253752981dc5695ab51712d36d7d
NickKuzmin <kuzmin.nick@rambler.ru>
2015-05-22 12:18:18 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Fixed bug with unworking links to UserStatisticsPage from office statistics table.

--- Portal/Portal/Statistics/OfficeStatistics.aspx.cs
+++ Portal/Portal/Statistics/OfficeStatistics.aspx.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Globalization;
 using UlterSystems.PortalLib.BusinessObjects;
 
 public partial class Statistics_OfficeStatistics : BaseWebPage
@@ -11,6 +11,21 @@ public partial class Statistics_OfficeStatistics : BaseWebPage
         if (!DateClass.TryParseRequestQueryDates(Request, out begin, out end))
             Response.Redirect(hlMain.NavigateUrl);
 
-        officeStat.ShowStatistics(begin, end);
+        var dateTimeFormatInfo = CultureInfo.CurrentCulture;
+        if (!DateTime.TryParse(begin.ToString(), dateTimeFormatInfo, DateTimeStyles.None, out begin))
+            return;
+        if (!DateTime.TryParse(end.ToString(), dateTimeFormatInfo, DateTimeStyles.None, out end))
+            return;
+
+        var beginDateStringInInvariantCulture = begin.ToString(CultureInfo.InvariantCulture);
+        var endDateStringInInvariantCulture = end.ToString(CultureInfo.InvariantCulture);
+        DateTime beginDateConvertedToInvariantCulture;
+        if (!DateTime.TryParse(beginDateStringInInvariantCulture, CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.None, out beginDateConvertedToInvariantCulture))
+            return;
+        DateTime endDateConvertedToInvariantCulture;
+        if (!DateTime.TryParse(endDateStringInInvariantCulture, CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.None, out endDateConvertedToInvariantCulture))
+            return;
+
+        officeStat.ShowStatistics(beginDateConvertedToInvariantCulture, endDateConvertedToInvariantCulture);
     }
 }
