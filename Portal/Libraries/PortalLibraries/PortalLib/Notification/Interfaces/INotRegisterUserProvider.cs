9610a28c62f77371f2a0b1c3df3d7dd7dfce4f47
prototype9449 <prototype9449@yandex.ru>
2015-06-01 19:28:09 +0300

    Confirmit/Portal#18  Class NotificationDelivery was almost separeted

---
+++ Portal/Libraries/PortalLibraries/PortalLib/Notification/Interfaces/INotRegisterUserProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UlterSystems.PortalLib.BusinessObjects;
+
+namespace ConfirmIt.PortalLib.Notification.Interfaces
+{
+    public interface INotRegisterUserProvider
+    {
+        IList<Person> GetNotRegisterUsers(DateTime datetime);
+        IList<Person> GetUsersWithShortMainWork(DateTime datetime, TimeSpan minDurationMainWork);
+    }
+}
