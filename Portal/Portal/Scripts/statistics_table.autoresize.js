27b8362ec12a0da842831135ed9f19c3b66f85c0
NickKuzmin <kuzmin.nick@rambler.ru>
2015-06-03 16:45:07 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Fixed bug with uncorrect rendering Office Statistics table in Google Chrome.

--- Portal/Portal/Scripts/statistics_table.autoresize.js
+++ Portal/Portal/Scripts/statistics_table.autoresize.js
@@ -11,6 +11,6 @@ function initializeAutoResize() {
     document.getElementsByClassName("customTable")[0].style.width = (clientWidth - 250) * 0.8 + "px";
     document.getElementById("updatedTable").style.marginLeft = clientWidth * 0.1 + "px";
 
-    document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.8 - 140 + "px";
-    document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.8 - 140 + "px";
+    document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px";
+    document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px";
 }
