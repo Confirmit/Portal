cf3ea3a0d106b9c5dfb805eeea9930efaf833c39
NickKuzmin <kuzmin.nick@rambler.ru>
2015-06-03 17:01:21 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Refactored script of office-statistics table autoresize.

--- Portal/Portal/Scripts/statistics_table.autoresize.js
+++ Portal/Portal/Scripts/statistics_table.autoresize.js
@@ -6,11 +6,11 @@
 }());
 
 function initializeAutoResize() {
-    var clientWidth = document.getElementById("updatedTable").clientWidth; //container
-    document.getElementsByClassName("customHeader")[0].style.width = (clientWidth - 250) * 0.8 + "px";
-    document.getElementsByClassName("customTable")[0].style.width = (clientWidth - 250) * 0.8 + "px";
-    document.getElementById("updatedTable").style.marginLeft = clientWidth * 0.1 + "px";
+    var updatedTableWidth = document.getElementById("updatedTable").clientWidth; //container
+    document.getElementsByClassName("customHeader")[0].style.width = (updatedTableWidth - 250) * 0.8 + "px"; // 250 - width of first column
+    document.getElementsByClassName("customTable")[0].style.width = (updatedTableWidth - 250) * 0.8 + "px";
+    document.getElementById("updatedTable").style.marginLeft = updatedTableWidth * 0.1 + "px";
 
-    document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px";
+    document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px"; // total height of header and footer
     document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px";
 }
