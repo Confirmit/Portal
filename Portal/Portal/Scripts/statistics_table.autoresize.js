bdae5501649c200e54e68fb37d04819b1e4c4737
NickKuzmin <kuzmin.nick@rambler.ru>
2015-06-03 08:56:17 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Changed script of statistics table autoresize for executing by page-load.

--- Portal/Portal/Scripts/statistics_table.autoresize.js
+++ Portal/Portal/Scripts/statistics_table.autoresize.js
@@ -1,11 +1,16 @@
-(function () {
-      window.onresize = function () {
-            var clientWidth = document.getElementById("updatedTable").clientWidth; //container
-            document.getElementsByClassName("customHeader")[0].style.width = (clientWidth - 250) * 0.8 + "px";
-            document.getElementsByClassName("customTable")[0].style.width = (clientWidth - 250) * 0.8 + "px";
-            document.getElementById("updatedTable").style.marginLeft = clientWidth * 0.1 + "px";
-
-            document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.6 + "px";
-            document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.6 + "px";
-        };
+(function() {
+    window.onresize = function() {
+        initializeAutoResize();
+    };
+    initializeAutoResize();
 }());
+
+function initializeAutoResize() {
+    var clientWidth = document.getElementById("updatedTable").clientWidth; //container
+    document.getElementsByClassName("customHeader")[0].style.width = (clientWidth - 250) * 0.8 + "px";
+    document.getElementsByClassName("customTable")[0].style.width = (clientWidth - 250) * 0.8 + "px";
+    document.getElementById("updatedTable").style.marginLeft = clientWidth * 0.1 + "px";
+
+    document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.6 + "px";
+    document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.6 + "px";
+}
