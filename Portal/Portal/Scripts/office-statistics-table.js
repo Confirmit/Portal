d70d9c94428a35f1df76f14c481a0fc56a47f7d3
NickKuzmin <kuzmin.nick@rambler.ru>
2015-06-10 12:46:18 +0300

    Confirmit/Portal#54 [RedirectionToUsersStatisticInformationPanel] Added constants for correct positioning of statistics table.

--- Portal/Portal/Scripts/office-statistics-table.js
+++ Portal/Portal/Scripts/office-statistics-table.js
@@ -20,9 +20,9 @@ function addDoubleScrollHandler(element) {
 function initializeAutoResize() {
     var updatedTableWidth = document.getElementById("updatedTable").clientWidth; //container
     //var updatedTableWidth = document.body.clientWidth;
-    document.getElementsByClassName("customHeader")[0].style.width = (updatedTableWidth - 252) * 0.8 - 16 + "px"; // 250 - width of first column
-    document.getElementsByClassName("customTable")[0].style.width = (updatedTableWidth - 252) * 0.8 + "px";
-    document.getElementById("updatedTable").style.marginLeft = updatedTableWidth * 0.1 + "px";
+    document.getElementsByClassName("customHeader")[0].style.width = (updatedTableWidth) * 0.8 - 16 - 252 + "px"; // 250 - width of first column
+    document.getElementsByClassName("customTable")[0].style.width = (updatedTableWidth) * 0.8 - 252 + "px";
+    document.getElementById("updatedTable").style.marginLeft = (updatedTableWidth - 252) * 0.2 + 16 + "px";
 
     document.getElementsByClassName("customTable")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px"; // total height of header and footer
     document.getElementsByClassName("firstColumn")[0].style.height = (document.body.clientHeight) * 0.8 - 150 + "px";
