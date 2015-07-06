a46e941473a054d6e2776da69edfe0b07f7a426e
prototype9449 <prototype9449@yandex.ru>
2015-07-06 16:00:07 +0300

    Confirmit/Portal#45 Tests were improved

--- Portal/TestOfImplementersOfRules/Tests/RuleExecutorTests.cs
+++ Portal/TestOfImplementersOfRules/Tests/RuleExecutorTests.cs
@@ -7,6 +7,7 @@ using ConfirmIt.PortalLib.BusinessObjects.RuleEnities.Processor;
 using ConfirmIt.PortalLib.BusinessObjects.RuleEnities.Repositories.Interfaces;
 using ConfirmIt.PortalLib.BusinessObjects.RuleEnities.Rules;
 using ConfirmIt.PortalLib.BusinessObjects.RuleEnities.Utilities;
+using ConfirmIt.PortalLib.Notification;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestOfImplementersOfRules.CommonTestClasses;
 using TestOfImplementersOfRules.CommonTestClasses.TestRepositories;
@@ -14,11 +15,13 @@ using TestOfImplementersOfRules.Factories;
 using TestOfImplementersOfRules.Factories.FilterFactories;
 using TestOfImplementersOfRules.Factories.TimeEntityFactories;
 using TestOfImplementersOfRules.Helpers;
+using TestSendingNotRegisterUsers.Test_classes;
+using UlterSystems.PortalLib.Notification;
 
 namespace TestOfImplementersOfRules.Tests
 {
     [TestClass]
-    public class RuleExectuterTests
+    public class RuleExecutorTests
     {
         public IRuleInstanceRepository RuleInstanceRepository;
 
@@ -33,7 +36,7 @@ namespace TestOfImplementersOfRules.Tests
         }
 
         [TestMethod]
-        public void NotifyLastUserExecutor_ExecuteRule_ResultShouldBeTrueWithNotifyLastUserRule()
+        public void NotifyLastUserExecutor_ExecuteRule_ResultShouldBeTrue()
         {
             const int countRules = 5;
             const int countGroups = 5;
@@ -66,7 +69,7 @@ namespace TestOfImplementersOfRules.Tests
         }
 
         [TestMethod]
-        public void NotifyLastUserExecutor_ExecuteRule_ResultShouldBeFalseWithNotifyLastUserRule()
+        public void NotifyLastUserExecutor_ExecuteRule_ResultShouldBeFalse()
         {
             const int countRules = 5;
             const int countGroups = 5;
@@ -95,6 +98,51 @@ namespace TestOfImplementersOfRules.Tests
 
             Assert.IsTrue(result);
             Assert.AreEqual("", messageHelper.Body);
+        }
+        
+        [TestMethod]
+        public void NotifyByTimeExecuter_ExecuteRule_ResultSholdBeTrue()
+        {
+            const int countRules = 1;
+            const int countGroups = 1;
+            const int countUsers = 1;
+
+            var ruleFactory = new RuleFactory();
+            var timeEntityFactory = new TimeEntityFactory();
+
+            var timeEntities = timeEntityFactory.GetTimeEntities(countRules, timeEntityFactory.GetDefaultTimeEntity);
+            var createdRules = ruleFactory.GetNotifyByTimeRules(timeEntities);
+
+            InitializeComponents(countGroups, countUsers, createdRules);
+
+            var testMailStorage = new TestMailStorage();
+            var mailProvider = new MailProvider("FromAddress", MailTypes.NRNotification, testMailStorage);
+            var notifyByTimeExecutor = new NotifyByTimeRuleExecutor(mailProvider, RuleInstanceRepository);
+
+            var rule = RuleInstanceRepository.RuleRepository.GetAllRulesByType<NotifyByTimeRule>().First();
+            var result = notifyByTimeExecutor.ExecuteRule(rule, new RuleInstance(rule));
+
+            var user = RuleInstanceRepository.RuleRepository.GetAllUsersByRule(rule.ID.Value).Single();
+
+            var mail = new MailItem
+            {
+                Body = rule.Information,
+                Subject = rule.Subject,
+                FromAddress = "FromAddress",
+                ToAddress = user.PrimaryEMail
+            };
+            Assert.IsTrue(result);
+            Assert.IsTrue(testMailStorage.IsSave);
+            Assert.AreEqual(1, testMailStorage.CountSavingLetters);
+            Assert.IsTrue(AreEqual(mail, testMailStorage.GetMails(false).Single()));
+        }
+
+        private bool AreEqual(MailItem mail1, MailItem mail2)
+        {
+            return mail1.Body == mail2.Body &&
+                   mail1.Subject == mail2.Subject &&
+                   mail1.FromAddress == mail2.FromAddress &&
+                   mail1.ToAddress == mail2.ToAddress;
         }
     }
 }
--- Portal/TestSendingNotRegisterUsers/Test classes/TestMailStorage.cs
+++ Portal/TestSendingNotRegisterUsers/Test classes/TestMailStorage.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ConfirmIt.PortalLib.Notification;
 
-namespace TestSendingNotRegisterUsers
+namespace TestSendingNotRegisterUsers.Test_classes
 {
-    class TestMailStorage : IMailStorage
+    public class TestMailStorage : IMailStorage
     {
         public int CountSavingLetters;
         public bool IsSave;
